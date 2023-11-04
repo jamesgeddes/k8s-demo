b92fad50b69bf2343f8bed6dcdeb2346b525012d
James Geddes <j@jamesgeddes.pro>
2023-11-04 13:52:46 +0000

    create dockerhub repo

---
+++ terraform/dockerhub.tf
@@ -0,0 +1,9 @@
+resource "dockerhub_repository" "services" {
+  for_each = local.services
+
+  name             = "${var.project}-each.value"
+  namespace        = var.dockerhub_namespace
+  description      = "Repository for the ${each.value} service."
+  full_description = "This repository contains Docker images for the ${each.value} service."
+  private          = false
+}
