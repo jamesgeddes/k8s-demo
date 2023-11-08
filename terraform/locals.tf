b9685f67202c5827530f7036f446ca7146a8e69d
James Geddes <j@jamesgeddes.pro>
2023-11-08 00:08:50 +0000

    add cats

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,9 @@
 locals {
-  env          = terraform.workspace
-  cluster_name = "eks-${local.env}"
+  resource_prefix      = "${var.gtld}-${var.project}-${var.environment}"
+  state_bucket         = "${var.repo_owner}-terraform-state"
+  services             = toset(split(",", var.services))
+  application_set_yaml = file("${path.module}/../argocd/application-set.yaml")
+  application_set      = yamldecode(local.application_set_yaml)
+  domain               = "${var.project}.${var.gtld}"
+
 }
