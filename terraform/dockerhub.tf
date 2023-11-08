f77a4f4b5c702c7bfcc0ddf219a12d7f804dac2d
James Geddes <j@jamesgeddes.pro>
2023-11-08 00:07:11 +0000

    amend name and description

--- terraform/dockerhub.tf
+++ terraform/dockerhub.tf
@@ -1,9 +1,9 @@
 resource "dockerhub_repository" "services" {
   for_each = local.services
 
-  name             = "${var.project}-each.value"
+  name             = "${var.project}-${each.value}"
   namespace        = var.dockerhub_namespace
-  description      = "Repository for the ${each.value} service."
+  description      = "Repository for the ${var.project} ${each.value} service."
   full_description = "This repository contains Docker images for the ${each.value} service."
   private          = false
 }
