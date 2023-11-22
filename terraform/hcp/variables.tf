f73464b13ac6aa21883410a6508edafdd8e566b7
James Geddes <j@jamesgeddes.pro>
2023-11-22 23:35:27 +0000

    remove github

--- terraform/hcp/variables.tf
+++ terraform/hcp/variables.tf
@@ -2,7 +2,3 @@ variable "project" {
   description = "The name of the project that we are working on."
   type        = string
 }
-
-variable "repo_owner" {
-  description = "The org or individual who owns this repo"
-}
