707d6ff58fb8a50ef1bd3c2fc67a67fea66efbd3
James Geddes <j@jamesgeddes.pro>
2023-11-22 21:20:13 +0000

    separate out into distinct module

--- terraform/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  resource_prefix      = "${var.gtld}-${var.project}-${var.environment}"
-  state_bucket         = "${var.repo_owner}-terraform-state"
-  services             = toset(split(",", var.services))
-  application_set_yaml = file("${path.module}/../argocd/application-set.yaml")
-  application_set      = yamldecode(local.application_set_yaml)
-  domain               = "${var.project}.${var.gtld}"
-
-}
